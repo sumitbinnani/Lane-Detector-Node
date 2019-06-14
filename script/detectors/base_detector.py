41325d62e13af6817eac832a3b711e02cdb07634
Sumit Binnani <sumit.binnani@gmail.com>
2019-06-13 23:21:28 -0700

    Added LaneNet Detector and Pretrained Model

--- script/detectors/base_detector.py
+++ script/detectors/base_detector.py
@@ -1,25 +1,33 @@
-try:
-  from abc import ABC
-except:
-  from abc import ABCMeta as ABC
-from abc import abstractmethod
+from abc import ABCMeta, abstractmethod
 
+import cv2
 import numpy as np
 
-class LaneDetector(ABC):
-    COLOR_MAP = [np.array([255, 0, 0]),
-                 np.array([0, 255, 0]),
-                 np.array([0, 0, 255]),
-                 np.array([125, 125, 0]),
-                 np.array([0, 125, 125]),
-                 np.array([125, 0, 125]),
-                 np.array([50, 100, 50]),
-                 np.array([100, 50, 100])]
+
+class LaneDetector(object):
+    __metaclass__ = ABCMeta
+    COLOR_MAP = [[255, 0, 0],
+                 [0, 255, 0],
+                 [0, 0, 255],
+                 [125, 125, 0],
+                 [0, 125, 125],
+                 [125, 0, 125],
+                 [50, 100, 50],
+                 [100, 50, 100]]
 
     @abstractmethod
     def get_lanes(self, img):
         pass
-        
+
     @staticmethod
-    def draw_lanes(lane_coords, shape):
-        pass
+    def draw_lanes(lanes, shape):
+        mask_image = np.zeros(shape=(shape[0], shape[1], 3), dtype=np.uint8)
+
+        for i, xy_ in enumerate(lanes):
+            color = LaneDetector.COLOR_MAP[i % len(LaneDetector.COLOR_MAP)]
+            cv2.polylines(mask_image, xy_.reshape([-1, 1, 2]).astype(int), True, (color[0], color[1], color[2]))
+
+        cv2.imshow('frame', mask_image)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            exit()
